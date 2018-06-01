61e73e093011817c8a71e0ead30f3b53c1cda193
Aku Rex <akucodes@gmail.com>
2018-05-31 23:31:56 -0500

    adds test locale messages and display on page

--- src/main.js
+++ src/main.js
@@ -4,12 +4,32 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
-Vue.use(VueI18n)
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
+
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world in english'
+    }
+  },
+  de: {
+    message: {
+      hello: 'Maus! Los gehts!'
+    }
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages // set locale messages
+})
 
 /* eslint-disable no-new */
 new Vue({
+  i18n,
   el: '#app',
   router,
   components: { App },
