127f3fa404c11ff10410bb53d34fb22610f81891
Aku Rex <akucodes@gmail.com>
2018-06-01 01:26:19 -0500

    adds vue-bootstrap and a route

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.use(VueI18n)
 Vue.config.productionTip = false
 
